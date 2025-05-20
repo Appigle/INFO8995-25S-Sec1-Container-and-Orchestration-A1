8ab15bc406427cfde5ca876b9b088a8aed3058b3
Ray Chen <ray.chenley.up@gmail.com>
2025-05-20 16:39:39 -0400

    feat: update project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,39 @@
 <script setup>
-import { ref } from 'vue'
+import { LottieAnimation } from 'lottie-web-vue';
+import { ref } from 'vue';
+import animationData from '../assets/lottie_animation.json';
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div class="lottie-container">
+      <LottieAnimation
+        :animation-data="animationData"
+        :loop="true"
+        :auto-play="true"
+        :speed="1"
+      />
+    </div>
+    <div class="card">
+      <button type="button" @click="count++">count is {{ count }}</button>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
+.lottie-container {
+  width: 300px;
+  height: 300px;
+  margin: 0 auto;
+}
+
 .read-the-docs {
   color: #888;
 }
